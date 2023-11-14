d2c67d2c9b50822698c0f73efd9056b48aa7fece
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2023-11-14 08:57:20 +0530

    Update 6.First Fit.py

--- SPOS/Python/First Fit.py
+++ SPOS/Python/First Fit.py
@@ -4,39 +4,32 @@ class MemoryBlock:
         self.allocated = allocated
         self.process_id = process_id
 
-def first_fit(memory_blocks, process_size):
+def first_fit(memory_blocks, process_size, process_name):
     for block in memory_blocks:
         if not block.allocated and block.size >= process_size:
             block.allocated = True
-            block.process_id = "P"  # You can use the actual process ID here
+            block.process_id = process_name
             return True
     return False
 
 def display_memory_status(memory_blocks):
     print("Memory Block\tSize\tStatus\tProcess ID")
     for i, block in enumerate(memory_blocks, start=1):
-        print(f"{i}\t\t\t{block.size}\t{'Allocated' if block.allocated else 'Free'}\t{block.process_id}")
+        print(f"{i}\t\t\t{block.size}\t\t{'Allocated' if block.allocated else 'Free'}\t\t\t{block.process_id}")
 
 if __name__ == "__main__":
-    # Example memory blocks
-    memory_blocks = [
-        MemoryBlock(100),
-        MemoryBlock(50),
-        MemoryBlock(200),
-        MemoryBlock(75),
-        MemoryBlock(300),
-    ]
+    
+    num_blocks = int(input("Enter the number of memory blocks: "))
+    memory_blocks = [MemoryBlock(int(input(f"Enter size for block {i+1}: "))) for i in range(num_blocks)]
 
-    # Example processes with their sizes
-    processes = [30, 100, 75, 130]
-
-    # Allocate memory for each process using First Fit
-    for process_size in processes:
-        success = first_fit(memory_blocks, process_size)
+    num_processes = int(input("Enter the number of processes: "))
+    for i in range(num_processes):
+        process_size = int(input(f"Enter size for process {i+1}: "))
+        process_name = input(f"Enter name for process {i+1}: ")
+        success = first_fit(memory_blocks, process_size, process_name)
         if success:
-            print(f"Allocated {process_size} units to process.")
+            print(f"Allocated {process_size} units to process {process_name}.")
         else:
-            print(f"Failed to allocate {process_size} units. No suitable block found.")
+            print(f"Failed to allocate {process_size} units. No suitable block found for process {process_name}.")
 
-    # Display final memory status
     display_memory_status(memory_blocks)
